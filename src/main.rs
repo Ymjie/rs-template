a3960c09745bf012cf355681236bccff77943f07
Vshex <25454375+Ymjie@users.noreply.github.com>
2025-04-22 18:23:07 +0800

    feat: add build workflow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another_test() {
+        assert_ne!(5 * 2, 11);
+    }
+}
